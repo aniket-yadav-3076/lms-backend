eb234aced54cc805b440a61277055d628b97a67c
aniket-yadav-3076 <aniket.yadav@octobit8.com>
2025-02-19 13:58:27 +0530

    created viewset for the model
    Signed-off-by: aniket-yadav-3076 <aniket.yadav@octobit8.com>

--- learning/views.py
+++ learning/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import learn
+from .serializers import learnSerializer
 
-# Create your views here.
+
+class learnViewSet(viewsets.ModelViewSet):
+    queryset = learn.objects.all()
+    serializer_class = learnSerializer
