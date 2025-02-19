5746838ab659859bf9df855b3a7da364975d869f
aniket-yadav-3076 <aniket.yadav@octobit8.com>
2025-02-19 13:55:12 +0530

    created model for database
    Signed-off-by: aniket-yadav-3076 <aniket.yadav@octobit8.com>

--- learning/models.py
+++ learning/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class learn(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.IntegerField()
+    discount = models.IntegerField()
